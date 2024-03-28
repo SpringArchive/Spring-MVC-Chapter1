d336c27727770493a7fc516756e7436be72fcf24
KoungQ <guu119@naver.com>
2024-03-28 16:49:21 +0900

    Section 2

--- src/main/java/hello/servlet/basic/request/RequestHeaderServlet.java
+++ src/main/java/hello/servlet/basic/request/RequestHeaderServlet.java
@@ -18,6 +18,7 @@ public class RequestHeaderServlet extends HttpServlet {
         printStartLine(request);
         printHeader(request);
         printHeaderUtils(request);
+        printEtc(request);
     }
 
     private static void printStartLine(HttpServletRequest request) {
@@ -37,6 +38,7 @@ public class RequestHeaderServlet extends HttpServlet {
         System.out.println();
     }
 
+    // Header 모든 정보 출력
     private void printHeader(HttpServletRequest request) {
         System.out.println("--- Headers - start ---");
 
@@ -83,6 +85,27 @@ public class RequestHeaderServlet extends HttpServlet {
         System.out.println("request.getCharacterEncoding() = " +
                 request.getCharacterEncoding());
         System.out.println("--- Header 편의 조회 end ---");
+        System.out.println();
+    }
+
+    // 기타 정보
+    private void printEtc(HttpServletRequest request) {
+        System.out.println("--- 기타 조회 start ----");
+
+        // 요청이 온 것에 대한 정보
+        System.out.println("[Remote 정보]");
+        System.out.println("request.getRemoteHost() = " + request.getRemoteHost());
+        System.out.println("request.getRemoteAddr() = " + request.getRemoteAddr());
+        System.out.println("request.getRemotePort() = " + request.getRemotePort());
+        System.out.println();
+
+        // 내 서버 정보
+        System.out.println("[Local 정보]");
+        System.out.println("request.getLocalName() = " + request.getLocalName());
+        System.out.println("request.getLocalAddr() = " + request.getLocalAddr());
+        System.out.println("request.getLocalPort() = " + request.getLocalPort());
+
+        System.out.println("--- 기타 조회 end ---");
         System.out.println();
     }
 }
